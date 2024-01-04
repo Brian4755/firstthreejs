c6b707200927fea4191d5acb3a26461f5bbec755
Brian4755 <brian.mccune1@yahoo.com>
2024-01-04 17:12:37 -0600

    object slow rotation animation

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshBasicMaterial( {color: 0xFF6347, wireframe: true});
@@ -23,6 +23,10 @@ scene.add(torus);
 
 function animate() {
   requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
   renderer.render(scene, camera);
 }
 
