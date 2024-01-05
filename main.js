c5d911dbcf658be1c06c1723e086cac244bfe322
Brian4755 <brian.mccune1@yahoo.com>
2024-01-05 00:45:24 -0600

    light intensity for objeft increasae

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
+renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 // const geometry = new THREE.SphereGeometry(15,50,16);
@@ -22,11 +23,12 @@ const torus = new THREE.Mesh(geometry, material);
 
 scene.add(torus);
 
-const pointLight = new THREE.PointLight(0xffffff)
+const pointLight = new THREE.PointLight(0xffffff, 100)
 pointLight.position.set(5,5,5)
 
 const ambientLight = new THREE.AmbientLight(0xffffff)
-scene.add(pointLight, ambientLight)
+scene.add(pointLight)
+
 
 function animate() {
   requestAnimationFrame(animate);
